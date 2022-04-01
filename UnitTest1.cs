2f05606fd84bb899224a385643fadc41b0577ad4
Raúl Villares <raulvillares@gmail.com>
2022-04-01 13:04:33 +0200

    working

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,7 +8,17 @@ namespace tcr
         [Fact]
         public void Test1()
         {
+            var result = Fibonacci.Fib(0);
+            
+            Assert.Equal(0, result);
+        }
+    }
 
+    public class Fibonacci
+    {
+        public static int Fib(int i)
+        {
+            return 0;
         }
     }
 }
